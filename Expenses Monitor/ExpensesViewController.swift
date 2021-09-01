9ca1539ca2283260a1b2a69b4a675a353140a17a
Talha Ghous <talhaghous125@gmail.com>
2021-09-01 23:02:00 +1000

    Initial commit

---
+++ Expenses Monitor/ExpensesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Expenses Monitor
+//
+//  Created by Talha Ghous on 28/4/18.
+//  Copyright © 2018 Talha Ghous. All rights reserved.
+//
+
+import UIKit
+
+class ExpensesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
